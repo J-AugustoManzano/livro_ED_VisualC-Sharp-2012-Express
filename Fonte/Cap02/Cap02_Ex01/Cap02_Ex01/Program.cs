57ca21472edfa65676f8dac951754d38360e7db6
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2021-05-17 17:30:25 -0300

    Add files via upload

---
+++ Fonte/Cap02/Cap02_Ex01/Cap02_Ex01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cap02_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string NOME;
+
+            Console.Write("Entre seu nome: ");
+            NOME = Console.ReadLine();
+            Console.WriteLine("Ola, " + NOME);
+
+            Console.WriteLine();
+            Console.Write("Tecle <Enter> para encerrar... ");
+            Console.ReadLine();
+
+        }
+    }
+}
