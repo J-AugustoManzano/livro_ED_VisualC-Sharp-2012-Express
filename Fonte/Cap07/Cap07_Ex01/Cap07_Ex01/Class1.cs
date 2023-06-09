bb49647e8fd09161d3a1fcc71e79e230732a8d38
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 22:24:22 -0300

    Add files via upload

---
+++ Fonte/Cap07/Cap07_Ex01/Cap07_Ex01/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cap07_Ex01
+{
+    class Class1
+    {
+        public string NOME;
+        public float[] NOTA = new float[4];
+
+    }
+}
