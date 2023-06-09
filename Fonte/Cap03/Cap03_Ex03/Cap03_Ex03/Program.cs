2aae13c72819422d7dbf4cdaf2085e6db8d97091
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 22:25:36 -0300

    Add files via upload

---
+++ Fonte/Cap03/Cap03_Ex03/Cap03_Ex03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cap03_Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int NUMERO;
+
+            Console.Write("Entre um valor: ");
+            NUMERO = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            if (NUMERO >= 20 && NUMERO <= 90)
+                Console.WriteLine("O valor esta entre 20 e 90");
+            else
+                Console.WriteLine("O valor nao esta entre 20 e 90");
+
+            Console.WriteLine();
+            Console.Write("Tecle <Enter> para encerrar... ");
+            Console.ReadLine();
+
+        }
+    }
+}
